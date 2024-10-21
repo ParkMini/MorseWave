20e01a6199c331817493b6b29c6bb9b91d455394
ParkMini <parkteammini@gmail.com>
2024-10-21 14:50:10 +0900

    Init

---
+++ src/main/java/kr/pah/morsewave/MorsewaveApplication.java
@@ -0,0 +1,13 @@
+package kr.pah.morsewave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorsewaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorsewaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/pah/morsewave/MorsewaveApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pah.morsewave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorsewaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
